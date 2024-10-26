3518fa4304e76b12295608d1d8055f74e6274104
kharisovilyas <91402961+kharisovilyas@users.noreply.github.com>
2024-10-26 15:39:15 +0300

    Добавлены cors

--- src/main/java/ru/ilcorp/neuro_test/utils/config/CorsConfig.java
+++ src/main/java/ru/ilcorp/neuro_test/utils/config/CorsConfig.java
@@ -3,19 +3,23 @@ package ru.ilcorp.neuro_test.utils.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*")); // Разрешить все источники
+        configuration.setAllowedMethods(List.of("*")); // Разрешить все методы
+        configuration.setAllowedHeaders(List.of("*")); // Разрешить все заголовки
+        configuration.setAllowCredentials(true); // Если необходимо разрешить куки
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*"); // Разрешить все источники (замените "*" на ваш домен, если это продакшн)
-        config.addAllowedMethod("*"); // Разрешить все HTTP-методы
-        config.addAllowedHeader("*"); // Разрешить все заголовки
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
