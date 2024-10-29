a3f4873aa27938ff6a611c8aa615f9309d744ba1
kharisovilyas <91402961+kharisovilyas@users.noreply.github.com>
2024-10-29 12:50:04 +0300

    Исправление CORS+Security - 4

--- src/main/java/ru/ilcorp/neuro_test/controllers/api/v1/RestUserController.java
+++ src/main/java/ru/ilcorp/neuro_test/controllers/api/v1/RestUserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,7 @@ public class RestUserController {
     }
 
     @GetMapping("/hello")
+    @CrossOrigin(origins = "*")  // Добавьте эту аннотацию временно
     public ResponseEntity<String> helloWorld(){
         return ResponseEntity.ok("Hello World");
     }
